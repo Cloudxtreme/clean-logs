7d532f5722b49c810855c3697cc90f609aa6203d
dn365 <duning3650@gmail.com>
2012-08-28 20:32:08 +0800

    test

--- recipes/custom.rb
+++ recipes/custom.rb
@@ -166,19 +166,19 @@ node_logs_arry.each do |you|
 end
 
 
-
-directory "/var/chef/exec" do
-  action :create
+if rm_arry.flatten![-1] || archive_arry.flatten![-1]
+  directory "/var/chef/exec" do
+    action :create
+  end
+  template "/var/chef/exec/clean-log.sh" do
+    source "clean-log.sh.erb"
+    mode 0755
+    variables(
+        :config_rm => rm_arry.flatten!,
+        :config_archive => archive_arry.flatten!
+      )
+  end
 end
-template "/var/chef/exec/clean-log.sh" do
-  source "clean-log.sh.erb"
-  mode 0755
-  variables(
-      :config_rm => rm_arry.flatten!,
-      :config_archive => archive_arry.flatten!
-    )
-end
-
 
   
 
